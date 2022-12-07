cb19357b0a297170035dcb6757785b167299bf6c
Matvei P <pustoy.dev@gmail.com>
2022-12-08 01:27:27 +0300

    Linting and updating styles

--- gulp/compileStyles.mjs
+++ gulp/compileStyles.mjs
@@ -1,24 +1,43 @@
 import gulp from 'gulp';
+import replace from 'gulp-replace';
+import rename from 'gulp-rename';
+import gulpif from 'gulp-if';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import csso from 'gulp-csso';
 import gcmq from 'gulp-group-css-media-queries';
-import rename from 'gulp-rename';
+import atImport from 'postcss-import';
+import inlineSVG from 'postcss-inline-svg';
+import webpBg from 'webp-in-css/plugin.js';
 
 const sass = gulpSass(dartSass);
 
 const compileStyles = () =>
-  gulp.src('source/sass/style.scss', {sourcemaps: true})
-      .pipe(sass().on('error', sass.logError))
-      .pipe(postcss([autoprefixer({
-        grid: true,
-      })]))
-      .pipe(gcmq()) // выключите, если в проект импортятся шрифты через ссылку на внешний источник
-      .pipe(gulp.dest('build/css'))
+  gulp
+      .src('source/sass/entrypoint.scss', {sourcemaps: app.isDev})
+      .pipe(sass.sync(
+          gulpif(app.isProd, {outputStyle: 'compressed'})
+      ).on('error', sass.logError))
+      .pipe(replace(/@img\//g, '../img/'))
+      .pipe(postcss([
+        atImport(),
+        inlineSVG(),
+        webpBg({
+          addNoJs: true,
+        }),
+        autoprefixer({
+        // grid: true,
+        })
+      ]))
+      .pipe(gcmq())
+      .pipe(rename('style.css'))
+      .pipe(gulpif(app.isProd, gulp.dest('build/css')))
       .pipe(csso())
-      .pipe(rename('style.min.css'))
+      .pipe(rename({
+        extname: '.min.css',
+      }))
       .pipe(gulp.dest('build/css', {sourcemaps: '.'}));
 
 export default compileStyles;
